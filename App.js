04f5cc71f03662fa6331b6589acaf2cfecc6fb59
NiranDx <niran3688@gmail.com>
2018-09-23 20:23:13 +0700

    import/export components

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import React from 'react'; 
-import { StyleSheet, Text, View } from 'react-native'; 
- 
-export default class App extends React.Component { 
-  doIt = () => { 
-      console.log("Hello from console")   
-    }   
-  render() {     
-    return (      
-     <View style={styles.container}>         
-     <Text onPress={this.doIt}>Hello World NIRAN</Text>
+import Weather from './components/Weather'
 
-      </View>     
-    );  
+export default class App extends React.Component {
+   render() { 
+     return (
+     <Weather />
+     ); 
+    } 
   }
-} 
- 
-const styles = StyleSheet.create({   
-  container: {     
-    flex: 1,     
-    backgroundColor: '#fff',     
-    alignItems: 'center',     
-    justifyContent: 'center',   
-  }, 
-});
